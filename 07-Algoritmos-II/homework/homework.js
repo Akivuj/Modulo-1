d54b124ec5f839c4832ddfe4cb17675fb49c8dc8
Alberto Jauregui <alb.jauregui@gmail.com>
2022-05-14 10:53:33 -0500

    07

--- 07-Algoritmos-II/homework/homework.js
+++ 07-Algoritmos-II/homework/homework.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 // No cambies los nombres de las funciones.
 
 function quickSort(array) {
@@ -6,7 +6,20 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if (array.length <= 1) {
+    return array;
+  }
+  let pivot = array[0];
+  let left = [];
+  let right = [];
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+  return quickSort(left).concat(pivot, quickSort(right));
 }
 
 function mergeSort(array) {
@@ -14,7 +27,31 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  function merge(left, right) {
+    let arr = [];
+    // Break out of loop if any one of the array gets empty
+    while (left.length && right.length) {
+      // Pick the smaller among the smallest element of left and right sub arrays
+      if (left[0] < right[0]) {
+        arr.push(left.shift());
+      } else {
+        arr.push(right.shift());
+      }
+    }
 
+    // Concatenating the leftover elements
+    // (in case we didn't go through the entire left or right array)
+    return [...arr, ...left, ...right];
+  }
+  const half = array.length / 2;
+
+  // Base case or terminating case
+  if (array.length < 2) {
+    return array;
+  }
+
+  const left = array.splice(0, half);
+  return merge(mergeSort(left), mergeSort(array));
 }
 
 // No modificar nada debajo de esta línea
